fc185cd46cbacd78ce723db5a49a96afe72091b1
Shubhamsarkar98 <shubhamsarkar9435@gmail.com>
2024-03-15 12:25:16 +0530

    header css add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,45 +1,76 @@
 import React, { useState } from "react";
-import { faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { DateRange } from "react-date-range";
-import { format } from "date-fns";
-import { useNavigate } from "react-router-dom";
-import "react-date-range/dist/styles.css"; // main css file
-import "react-date-range/dist/theme/default.css"; // theme css file
+import { FaBed, FaCalendar, FaCar, FaDollyFlatbed } from "react-icons/fa";
+import { FaPerson } from "react-icons/fa6";
+import { MdFlight, MdOutlineLocalTaxi } from "react-icons/md";
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; 
+import 'react-date-range/dist/theme/default.css'; 
 import "./header.css";
 
-const Header = ({ type }) => {
-  const [destination, setDestination] = useState("");
-  const [openDate, setOpenDate] = useState(false);
-  const [date, setDate] = useState([
-    {
-      startDate: new Date(),
-      endDate: new Date(),
-      key: "selection",
-    },
-  ]);
-  const [openOptions, setOpenOptions] = useState(false);
-  const [options, setOptions] = useState({
-    adult: 1,
-    children: 0,
-    room: 1,
-  });
+const Header = () => {
 
-  const navigate = useNavigate();
 
-  const handleOption = (name, operation) => {
-    setOptions((prev) => ({
-      ...prev,
-      [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
-    }));
-  };
-
-  const handleSearch = () => {
-    navigate("/hotels", { state: { destination, date, options } });
-  };
 
   return (
     <>
+      <div className="header">
+        <div className="headerContainer">
+          <div className="headerList">
+            <div className="headerListItem active">
+              <FaBed />
+              <span>Stays</span>
+            </div>
+
+            <div className="headerListItem">
+              <MdFlight />
+              <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+              <FaCar />
+              <span>Car rentals</span>
+            </div>
+            <div className="headerListItem">
+              <FaDollyFlatbed />
+              <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+              <MdOutlineLocalTaxi />
+              <span>Airport taxis</span>
+            </div>
+          </div>
+          <h1 className="headerTitle">
+            A lifetime of discounts? It's Genius.
+          </h1>
+          <p className="headerDesc">
+            Get rewarded for your travels – unlock instant savings of 10% or
+            more with a free Lamabooking account
+          </p>
+          <button className="headerBtn">Sign in / Register</button>
+           <div className="headerSearch">
+              <div className="headerSearchItem">
+                <FaBed className="headerIcon" />
+                <input
+                  type="text"
+                  placeholder="Where are you going?"
+                  className="headerSearchInput"
+                />
+              </div>
+              <div className="headerSearchItem">
+                <FaCalendar className="headerIcon" />
+               <spen className ="headerSearchText">date to date</spen>
+              </div>
+              <div className="headerSearchItem">
+                <FaPerson className="headerIcon" />
+                <spen className ="headerSearchText">2 adults 2 Children 1 room </spen>
+              </div>
+              <div className="headerSearchItem">
+                <button className="headerBtn">
+                  Search
+                </button>
+              </div>
+            </div>
+        </div>
+      </div>
     </>
   );
 };
