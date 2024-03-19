6bbd4e315ad81fdbc2cbe5cdc7ce03eeebc6bcaf
Shubhamsarkar98 <shubhamsarkar9435@gmail.com>
2024-03-19 11:31:13 +0530

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import Navbar  from '../components/Navbar'
+import Navbar from '../components/Navbar'
 import Header from '../components/Header/Header'
+import Featured from '../components/featured/Featured'
+import PropertyList from '../components/propertyList/PropertyList'
 const Home = () => {
   return (
-    <div><Navbar/>
-    <Header/>
+    <div><Navbar />
+      <Header />
+      <Featured/>
+      <PropertyList type={'rt'}/>
     </div>
   )
 }
